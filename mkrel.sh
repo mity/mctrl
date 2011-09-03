051dd152ae15c2c4157989c6bbc6ee3b4efb7187
Morous <mity@morous.org>
2011-09-03 20:28:03 +0200

    [mkrel.sh] Rewrite the release build script.
     * mkrel.sh now considers the HEAD in git repository as the main source
       to build from (instead of the local directory tree, which can be
       polluted by many temporary files or uncommited modifications.
    
     * Only two ZIP packages are now generated. Source package corresponding
       to the 'git archive' command and binary, which contains 32-bit and
       64-bit prebuilt DLLs, import libraries, examples and also generated
       documentation.

--- mkrel.sh
+++ mkrel.sh
@@ -9,11 +9,16 @@
 # All packages are put in current directory (and overwritten if exist already).
 
 
+# We want to control what's going to stdout/err.
+exec 3>&1
+exec 1>/dev/null 2>&1
+
 CWD=`pwd`
+PRJ="$CWD"
 
 # Check we run this script from the right diretory:
-if [ ! -x $CWD/mkrel.sh ]; then
-    echo You have to run mkrel.sh script from the main mCtrl directory.
+if [ ! -x $PRJ/mkrel.sh ]; then
+    echo "You have to run mkrel.sh script from the main mCtrl directory." >&3
     exit 1
 fi
 
@@ -22,7 +27,7 @@ if [ x$TMP = x ]; then
     TMP=/tmp
 fi
 if [ ! -d $TMP ]; then
-    echo Directory '$TMP' does not exist.
+    echo "Directory '$TMP' does not exist." >&3
     exit 1
 fi
 
@@ -31,213 +36,194 @@ fi
 # Detect version #
 ##################
 
-echo -n "Detecting mCtrl version... "
+echo -n "Detecting mCtrl version... " >&3
 VERSION_MAJOR=`grep VERSION_MAJOR src/version.h | head -1 | sed "s/\#def.*MAJOR\ *//"`
 VERSION_MINOR=`grep VERSION_MINOR src/version.h | head -1 | sed "s/\#def.*MINOR\ *//"`
 VERSION_RELEASE=`grep VERSION_RELEASE src/version.h | head -1 | sed "s/\#def.*RELEASE\ *//"`
 VERSION=$VERSION_MAJOR.$VERSION_MINOR.$VERSION_RELEASE
 if [ x$VERSION = x ]; then
-    echo "Failed."
+    echo "Failed." >&3
     exit 1
 fi
-echo "$VERSION"
+echo "$VERSION" >&3
 
 
 #####################
 # Detect a zip tool #
 #####################
 
-echo -n "Detecting zip archiver... "
-if test `which 7za 2>> /dev/null`; then
+echo -n "Detecting zip archiver... " >&3
+if which 7za; then
     MKZIP="7za a -r -mx9"
-elif test `which 7z 2>> /dev/null`; then
+elif which 7z; then
     MKZIP="7z a -r -mx9"
-elif test `which zip 2>> /dev/null`; then
+elif which zip; then
     MKZIP="zip -r"
 else
-    echo "not found."
+    echo "Not found." >&3
     exit 1
 fi
-echo $MKZIP
+echo "$MKZIP" >&3
 
 
 ##################
 # Detect lib.exe #
 ##################
 
-echo -n "Detecting lib.exe... "
-if test `which lib.exe 2>> /dev/null`; then
+# 1. Try lib.exe in $PATH
+# 2. Try default install place(s) for MSVC 10.0
+# 3. Try default install place(s) for any MSVC version
+
+echo -n "Detecting lib.exe... " >&3
+if which lib.exe ; then
     LIBEXE="lib.exe"
-    echo "$LIBEXE"
 elif [ -x "/c/Program Files/Microsoft Visual Studio 10.0/VC/bin/amd64/lib.exe" ]; then 
     LIBEXE="/c/Program Files/Microsoft Visual Studio 10.0/VC/bin/amd64/lib.exe"
-    echo "$LIBEXE"
 elif [ -x "/c/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin/amd64/lib.exe" ]; then
     LIBEXE="/c/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin/amd64/lib.exe"
-    echo "$LIBEXE"
 else
-    echo "not found."
+    LIBEXE=`ls /c/Program\ Files*/Microsoft\ Visual\ Studio*/VC/bin/amd64/lib.exe | head -1`
+fi
+if [ "x$LIBEXE" = x ]; then
+    echo "Not found: MSVC import libs won't be generated" >&3
+else
+    echo "$LIBEXE" >&3
 fi
 
 
-#############################
-# Prepare build environment #
-#############################
+#########################
+# Build 64-bit binaries #
+#########################
 
-echo -n "Preparing build environment in $TMP... "
-rm -f mCtrl-$VERSION-*.zip
 rm -rf $TMP/mCtrl-$VERSION
-rm -rf $TMP/mCtrl-$VERSION-src
-rm -rf $TMP/mCtrl-$VERSION-x86
-rm -rf $TMP/mCtrl-$VERSION-x86_64
-cp -rf . $TMP/mCtrl-$VERSION
-(cd $TMP/mCtrl-$VERSION  &&  rm -rf doc/*)
-(cd $TMP/mCtrl-$VERSION  &&  rm -f mCtrl*.zip)
-(cd $TMP/mCtrl-$VERSION  &&  rm -f build*.log)
-(cd $TMP/mCtrl-$VERSION  &&  make distclean >> /dev/null)
-find $TMP/mCtrl-$VERSION/obj -type f -name \*.o -exec rm {} \;
-find $TMP/mCtrl-$VERSION/obj -type f -name \*.def -exec rm {} \;
-(cd $TMP/mCtrl-$VERSION  &&  rm -rf .git)
-find $TMP/mCtrl-$VERSION -type f -name .gitignore -exec rm -rf {} \;
-echo "Done."
+git clone . $TMP/mCtrl-$VERSION
+
+echo -n "Building 64-bit binaries... " >&3
+if which x86_64-w64-mingw32-gcc; then
+    x86_64-w64-mingw32-gcc -v > $CWD/build-x86_64.log 2>&1
+    echo >> $CWD/build-x86_64.log
+    (cd $TMP/mCtrl-$VERSION && make DEBUG=0 PREFIX=x86_64-w64-mingw32- all examples >> $CWD/build-x86_64.log 2>&1)
+    if [ $? -ne 0 ]; then
+        echo "Failed: see build-x86_64.log."
+        exit 1
+    fi
+    echo "Done." >&3
+    HAVE_X86_64=yes
+else
+    echo "Skipped: x86_64-w64-mingw32-gcc not not found in PATH." >&3
+fi
+
+if [ x$HAVE_X86_64 != x ]; then
+    echo -n "Create 64-bit MSVC import lib... " >&3
+    if [ "x$LIBEXE" != x ]; then
+        (cd $TMP/mCtrl-$VERSION && "$LIBEXE" /machine:x64 /def:obj/mCtrl.def /out:lib/mCtrl.lib >> $CWD/build-x86.log 2>&1)
+        echo "Done." >&3
+    else
+        echo "Skipped: lib.exe not found." >&3
+    fi
+    
+    mv $TMP/mCtrl-$VERSION/bin $TMP/mCtrl-$VERSION/bin64
+    mv $TMP/mCtrl-$VERSION/lib $TMP/mCtrl-$VERSION/lib64
+    (cd $TMP/mCtrl-$VERSION && make distclean)
+    mkdir $TMP/mCtrl-$VERSION/bin
+    mkdir $TMP/mCtrl-$VERSION/lib
+fi
+
+
+#########################
+# Build 32-bit binaries #
+#########################
+
+echo -n "Building 32-bit binaries... " >&3
+if which i686-w64-mingw32-gcc; then
+    i686-w64-mingw32-gcc -v > $CWD/build-x86.log 2>&1
+    echo >> $CWD/build-x86.log
+    (cd $TMP/mCtrl-$VERSION && make DEBUG=0 PREFIX=i686-w64-mingw32- all examples >> $CWD/build-x86.log 2>&1)
+    if [ $? -ne 0 ]; then
+        echo "Failed: see build-x86.log."
+        exit 1
+    fi
+    echo "Done." >&3
+    HAVE_X86=yes
+else
+    echo "Skipped: i686-w64-mingw32-gcc not not found in PATH." >&3
+fi
+
+if [ x$HAVE_X86 != x ]; then
+    echo -n "Create 32-bit MSVC import lib... " >&3
+    if [ "x$LIBEXE" != x ]; then
+        (cd $TMP/mCtrl-$VERSION && "$LIBEXE" /machine:x86 /def:obj/mCtrl.def /out:lib/mCtrl.lib >> $CWD/build-x86.log 2>&1)
+        echo "Done." >&3
+    else
+        echo "Skipped: lib.exe not found." >&3
+    fi
+fi
+
+
+##########################
+# Generate documentation #
+##########################
+
+echo -n "Generate documentation... " >&3
+if `which doxygen`; then
+    (cd $TMP/mCtrl-$VERSION && ( cat Doxyfile ; echo "PROJECT_NUMBER=$VERSION" ) | doxygen - > $CWD/build-doc.log 2>&1)
+    if [ $? -ne 0 ]; then
+        echo "Failed: See build-doc.log."
+        exit 1
+    fi
+    HAVE_DOC=yes
+    echo "Done." >&3
+else
+    echo "Skipped: doxygen not found in PATH." >&3
+fi
 
 
 ###############################
 # Make mCtrl-$VERSION-src.zip #
 ###############################
 
-echo -n "Pack source package... "
-$MKZIP mCtrl-$VERSION-src.zip $TMP/mCtrl-$VERSION >> /dev/null
+echo -n "Packing source package... " >&3
+git archive --prefix=mCtrl-$VERSION/ --output=mCtrl-$VERSION-src.zip HEAD
 if [ $? -ne 0 ]; then
-    echo "Failed."
+    echo "Failed." >&3
     exit 1
 fi
-echo "Done."
+echo "Done." >&3
 
+
+###############################
+# Make mCtrl-$VERSION-bin.zip #
+###############################
+
+echo -n "Packing binary package... " >&3
+rm -rf $TMP/mCtrl-$VERSION-src
 mv $TMP/mCtrl-$VERSION $TMP/mCtrl-$VERSION-src
-
-
-###############################
-# Make mCtrl-$VERSION-doc.zip #
-###############################
-
-echo -n "Generating doc package... "
-if test `which doxygen 2>> /dev/null`; then
-    (cd $TMP/mCtrl-$VERSION-src && ( cat Doxyfile ; echo "PROJECT_NUMBER=$VERSION" ) | doxygen - > $CWD/build-doc.log 2>&1)
-    if [ $? -ne 0 ]; then
-        echo "Failed: see build-doc.log."
-        exit 1
-    fi
-    HAVE_DOC=1
-    echo "Done."
-else
-    echo "Skipped: doxygen not found in PATH."
+mkdir $TMP/mCtrl-$VERSION
+if [ x$HAVE_X86 != x ]; then
+    cp -r $TMP/mCtrl-$VERSION-src/bin $TMP/mCtrl-$VERSION/
+    cp -r $TMP/mCtrl-$VERSION-src/lib $TMP/mCtrl-$VERSION/
+fi
+if [ x$HAVE_X86_64 != x ]; then
+    cp -r $TMP/mCtrl-$VERSION-src/bin64 $TMP/mCtrl-$VERSION/
+    cp -r $TMP/mCtrl-$VERSION-src/lib64 $TMP/mCtrl-$VERSION/
 fi
 if [ x$HAVE_DOC != x ]; then
-    echo -n "Pack doc package... "
-    mkdir $TMP/mCtrl-$VERSION
-    cp -rf $TMP/mCtrl-$VERSION-src/doc $TMP/mCtrl-$VERSION/doc
-    cp $TMP/mCtrl-$VERSION-src/AUTHORS $TMP/mCtrl-$VERSION/
-    cp $TMP/mCtrl-$VERSION-src/COPYING* $TMP/mCtrl-$VERSION/
-    cp $TMP/mCtrl-$VERSION-src/README $TMP/mCtrl-$VERSION/
-    $MKZIP mCtrl-$VERSION-doc.zip $TMP/mCtrl-$VERSION >> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "Failed."
-        exit 1
-    fi
-    echo "Done."
+    cp -r $TMP/mCtrl-$VERSION-src/doc $TMP/mCtrl-$VERSION/
 fi
-
-
-###############################
-# Make mCtrl-$VERSION-x86.zip #
-###############################
-
-echo -n "Building 32-bit package... "
-if test `which i686-w64-mingw32-gcc 2>> /dev/null`; then
-    (cd $TMP/mCtrl-$VERSION-src && make distclean >> /dev/null)
-    i686-w64-mingw32-gcc -v > $CWD/build-x86.log 2>&1
-    echo >> $CWD/build-x86.log
-    (cd $TMP/mCtrl-$VERSION-src && make DEBUG=0 PREFIX=i686-w64-mingw32- all examples >> $CWD/build-x86.log 2>&1)
-    if [ $? -ne 0 ]; then
-        echo "Failed: see build-x86.log."
-        exit 1
-    fi
-    HAVE_X86=1
-    echo "Done."
-else 
-    echo "Skipped: i686-w64-mingw32-gcc not not found in PATH."
+cp -r $TMP/mCtrl-$VERSION-src/include $TMP/mCtrl-$VERSION/
+cp $TMP/mCtrl-$VERSION-src/AUTHORS $TMP/mCtrl-$VERSION/
+cp $TMP/mCtrl-$VERSION-src/COPYING $TMP/mCtrl-$VERSION/
+cp $TMP/mCtrl-$VERSION-src/COPYING.lib $TMP/mCtrl-$VERSION/
+cp $TMP/mCtrl-$VERSION-src/README $TMP/mCtrl-$VERSION/
+find $TMP/mCtrl-$VERSION -name .git -exec rm -rf {} \;
+find $TMP/mCtrl-$VERSION -name .gitignore -exec rm {} \;
+$MKZIP mCtrl-$VERSION-bin.zip $TMP/mCtrl-$VERSION
+if [ $? -ne 0 ]; then
+    echo "Failed." >&3
+    exit 1
 fi
-if [ x$HAVE_X86 != x ]; then
-    echo -n "Create 32-bit MSVC import lib... "
-    if [ -n "$LIBEXE" ]; then
-        (cd $TMP/mCtrl-$VERSION-src && "$LIBEXE" /machine:x86 /def:obj/mCtrl.def /out:lib/mCtrl.lib) >> /dev/null
-        echo "Done."
-    else
-        echo "Skipped: missing lib.exe"
-    fi
-    
-    echo -n "Pack 32-bit package... "
-    rm -rf $TMP/mCtrl-$VERSION
-    mkdir $TMP/mCtrl-$VERSION
-    cp -rf $TMP/mCtrl-$VERSION-src/bin $TMP/mCtrl-$VERSION/bin
-    cp -rf $TMP/mCtrl-$VERSION-src/include $TMP/mCtrl-$VERSION/include
-    cp -rf $TMP/mCtrl-$VERSION-src/lib $TMP/mCtrl-$VERSION/lib
-    cp $TMP/mCtrl-$VERSION-src/AUTHORS $TMP/mCtrl-$VERSION/
-    cp $TMP/mCtrl-$VERSION-src/COPYING* $TMP/mCtrl-$VERSION/
-    cp $TMP/mCtrl-$VERSION-src/README $TMP/mCtrl-$VERSION/
-    $MKZIP mCtrl-$VERSION-x86.zip $TMP/mCtrl-$VERSION >> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "Failed."
-        exit 1
-    fi
-    echo "Done."
-fi
-
-
-##################################
-# Make mCtrl-$VERSION-x86_64.zip #
-##################################
-
-echo -n "Building 64-bit package... "
-if test `which x86_64-w64-mingw32-gcc 2>> /dev/null`; then
-    (cd $TMP/mCtrl-$VERSION-src && make distclean >> /dev/null)
-    x86_64-w64-mingw32-gcc -v > $CWD/build-x86_64.log 2>&1
-    echo >> $CWD/build-x86_64.log
-    (cd $TMP/mCtrl-$VERSION-src && make DEBUG=0 PREFIX=x86_64-w64-mingw32- all examples >> $CWD/build-x86_64.log 2>&1)
-    if [ $? -ne 0 ]; then
-        echo "Failed: see build-x86_64.log."
-        exit 1
-    fi
-    HAVE_X64=1
-    echo "Done."
-else
-    echo "Skipped: x86_64-w64-mingw32-gcc not found in PATH."
-fi
-if [ x$HAVE_X64 != x ]; then
-    echo -n "Create 64-bit MSVC import lib... "
-    if [ -n "$LIBEXE" ]; then
-        (cd $TMP/mCtrl-$VERSION-src && "$LIBEXE" /machine:x64 /def:obj/mCtrl.def /out:lib/mCtrl.lib) >> /dev/null
-        echo "Done."
-    else
-        echo "Skipped: missing lib.exe"
-    fi
-    
-    rm -rf $TMP/mCtrl-$VERSION
-    mkdir $TMP/mCtrl-$VERSION
-    cp -rf $TMP/mCtrl-$VERSION-src/bin $TMP/mCtrl-$VERSION/bin
-    cp -rf $TMP/mCtrl-$VERSION-src/include $TMP/mCtrl-$VERSION/include
-    cp -rf $TMP/mCtrl-$VERSION-src/lib $TMP/mCtrl-$VERSION/lib
-    cp $TMP/mCtrl-$VERSION-src/AUTHORS $TMP/mCtrl-$VERSION/
-    cp $TMP/mCtrl-$VERSION-src/COPYING* $TMP/mCtrl-$VERSION/
-    cp $TMP/mCtrl-$VERSION-src/README $TMP/mCtrl-$VERSION/
-    7z a mCtrl-$VERSION-x86_64.zip $TMP/mCtrl-$VERSION >> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "Failed."
-        exit 1
-    fi
-    echo "Done."
-fi
+rm -rf $TMP/mCtrl-$VERSION
+echo "Done." >&3
 
 
 ############
@@ -246,8 +232,6 @@ fi
 
 echo -n "Cleaning... "
 rm -rf $TMP/mCtrl-$VERSION
-rm -rf $TMP/mCtrl-$VERSION-src
-rm -rf $TMP/mCtrl-$VERSION-doc
-rm -rf $TMP/mCtrl-$VERSION-x86
-rm -rf $TMP/mCtrl-$VERSION-x86_64
+rm -rf $TMP/mCtrl-$VERSION-tmp
 echo "Done."
+
