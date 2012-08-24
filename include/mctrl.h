08b48d2f71129d9e47556812e7e3c5a32b75fcca
Morous <mity@morous.org>
2012-08-24 22:03:09 +0200

    [include/mctrl.h] Update copyright year.

--- include/mctrl.h
+++ include/mctrl.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Martin Mitas
+ * Copyright (c) 2011-2012 Martin Mitas
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
