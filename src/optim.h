a3a8d08d8b77010e611a603bec74eb75d3614213
Morous <mity@morous.org>
2012-09-04 21:56:10 +0200

    [src/optim.h] Fix indenation (tabs -> spaces).

--- src/optim.h
+++ src/optim.h
@@ -31,43 +31,43 @@
 static inline void
 mc_inlined_memcpy(void* addr0, const void* addr1, size_t n)
 {
-	BYTE* iter0 = (BYTE*) addr0;
-	const BYTE* iter1 = (const BYTE*) addr1;
+    BYTE* iter0 = (BYTE*) addr0;
+    const BYTE* iter1 = (const BYTE*) addr1;
 
-	while(n-- > 0)
-		*iter0++ = *iter1++;
+    while(n-- > 0)
+        *iter0++ = *iter1++;
 }
 
 static inline void
 mc_inlined_memmove(void* addr0, const void* addr1, size_t n)
 {
-	BYTE* iter0 = (BYTE*) addr0;
-	const BYTE* iter1 = (const BYTE*) addr1;
+    BYTE* iter0 = (BYTE*) addr0;
+    const BYTE* iter1 = (const BYTE*) addr1;
 
-	if(iter0 < iter1) {
-		while(n-- > 0)
-			*iter0++ = *iter1++;
-	} else {
-		iter0 += n;
-		iter1 += n;
-		while(n-- > 0)
-			*(--iter0) = *(--iter1);
-	}
+    if(iter0 < iter1) {
+        while(n-- > 0)
+            *iter0++ = *iter1++;
+    } else {
+        iter0 += n;
+        iter1 += n;
+        while(n-- > 0)
+            *(--iter0) = *(--iter1);
+    }
 }
 
 /* Swapping two memory blocks. They must not overlay. */
 static inline void
 mc_inlined_memswap(void* addr0, void* addr1, size_t n)
 {
-	BYTE* iter0 = (BYTE*) addr0;
-	BYTE* iter1 = (BYTE*) addr1;
-	BYTE tmp;
+    BYTE* iter0 = (BYTE*) addr0;
+    BYTE* iter1 = (BYTE*) addr1;
+    BYTE tmp;
 
-	while(n-- > 0) {
-		tmp = *iter0;
-		*iter0++ = *iter1;
-		*iter1++ = tmp;
-	}
+    while(n-- > 0) {
+        tmp = *iter0;
+        *iter0++ = *iter1;
+        *iter1++ = tmp;
+    }
 }
 
 #define mc_memswap    mc_inlined_memswap
@@ -83,62 +83,62 @@ mc_inlined_memswap(void* addr0, void* addr1, size_t n)
 static inline LONG
 mc_width(const RECT* r)
 {
-	return (r->right - r->left);
+    return (r->right - r->left);
 }
 
 static inline LONG
 mc_height(const RECT* r)
 {
-	return (r->bottom - r->top);
+    return (r->bottom - r->top);
 }
 
 static inline BOOL
 mc_contains(const RECT* r, const POINT* pt)
 {
-	return (r->left <= pt->x  &&  pt->x < r->right  &&
-	        r->top <= pt->y  &&  pt->y < r->bottom);
+    return (r->left <= pt->x  &&  pt->x < r->right  &&
+            r->top <= pt->y  &&  pt->y < r->bottom);
 }
 
 static inline BOOL
 mc_rect_is_empty(const RECT* r)
 {
-	return (r->left >= r->right  ||  r->top >= r->bottom);
+    return (r->left >= r->right  ||  r->top >= r->bottom);
 }
 
 static inline void
 mc_set_rect(RECT* r, LONG x0, LONG y0, LONG x1, LONG y1)
 {
-	r->left = x0;
-	r->top = y0;
-	r->right = x1;
-	r->bottom = y1;
+    r->left = x0;
+    r->top = y0;
+    r->right = x1;
+    r->bottom = y1;
 }
 
 static inline void
 mc_copy_rect(RECT* r0, const RECT* r1)
 {
-	r0->left = r1->left;
-	r0->top = r1->top;
-	r0->right = r1->right;
-	r0->bottom = r1->bottom;
+    r0->left = r1->left;
+    r0->top = r1->top;
+    r0->right = r1->right;
+    r0->bottom = r1->bottom;
 }
 
 static inline void
 mc_offset_rect(RECT* r, LONG dx, LONG dy)
 {
-	r->left += dx;
-	r->top += dy;
-	r->right += dx;
-	r->bottom += dy;
+    r->left += dx;
+    r->top += dy;
+    r->right += dx;
+    r->bottom += dy;
 }
 
 static inline void
 mc_inflate_rect(RECT* r, LONG dx, LONG dy)
 {
-	r->left -= dx;
-	r->top -= dy;
-	r->right += dx;
-	r->bottom += dy;
+    r->left -= dx;
+    r->top -= dy;
+    r->right += dx;
+    r->bottom += dy;
 }
 
 
