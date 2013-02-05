bb8cdcf356c6f600870534aeb047a718e8f79408
Morous <mity@morous.org>
2013-02-05 23:50:39 +0100

    [COMPAT] Fix roundf() MSVC workaround by inclusing <math.h>

--- src/compat.h
+++ src/compat.h
@@ -123,7 +123,8 @@
         #endif
     #endif
 
-     /* MSVC does not know roundf() */
+    /* MSVC does not know roundf() */
+	#include <math.h>
     static inline float roundf(float x)
         { return x >= 0.0f ? floorf(x + 0.5f) : ceilf(x - 0.5f); }
 #endif
