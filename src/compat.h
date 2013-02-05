6b6d93ba314e5c1c101cb79946f5bd1c5af8ad6e
Morous <mity@morous.org>
2013-02-05 22:45:46 +0100

    [COMPAT] Provide roundf() for MSVC.

--- src/compat.h
+++ src/compat.h
@@ -122,6 +122,10 @@
             #define SetWindowLongPtrW(win,ix,val)  SetWindowLongW((win),(ix),(LONG)(val))
         #endif
     #endif
+
+     /* MSVC does not know roundf() */
+    static inline float roundf(float x)
+        { return x >= 0.0f ? floorf(x + 0.5f) : ceilf(x - 0.5f); }
 #endif
 
 
