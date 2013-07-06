0c6b99d393f23a18c186c17988827abb4bec917d
Morous <mity@morous.org>
2013-07-06 15:52:53 +0200

    [version] Always use DLLVER_PLATFORM_NT.
    Even in ANSI build, we do not support Win9x for long time already,
    so DLLVER_PLATFORM_WINDOWS is wrong. (Although non-UNICODE builds
    are not really supported for some time too...)

--- src/version.c
+++ src/version.c
@@ -5,12 +5,12 @@
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -33,10 +33,10 @@ mcVersion(MC_VERSION* version)
 /* DllGetVersion accepts pointer to structure determined in runtime
  * by first DWORD (cbSize). */
 typedef union dllgetversioninfo_tag dllgetversioninfo_t;
-union dllgetversioninfo_tag { 
+union dllgetversioninfo_tag {
     DWORD cbSize;
     DLLVERSIONINFO info1;
-    DLLVERSIONINFO2 info2; 
+    DLLVERSIONINFO2 info2;
 };
 
 HRESULT MCTRL_API
@@ -48,16 +48,12 @@ DllGetVersion(dllgetversioninfo_t* dvi)
             dvi->info2.ullVersion = MAKEDLLVERULL(MC_VERSION_MAJOR,
                             MC_VERSION_MINOR, MC_VERSION_RELEASE, 0);
             /* no break, fall through */
-            
+
         case sizeof(DLLVERSIONINFO):
             dvi->info1.dwMajorVersion = MC_VERSION_MAJOR;
             dvi->info1.dwMinorVersion = MC_VERSION_MINOR;
             dvi->info1.dwBuildNumber = MC_VERSION_RELEASE;
-#ifdef UNICODE
             dvi->info1.dwPlatformID = DLLVER_PLATFORM_NT;
-#else
-            dvi->info1.dwPlatformID = DLLVER_PLATFORM_WINDOWS;
-#endif
             return S_OK;
 
         default:
