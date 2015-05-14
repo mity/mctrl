39a3aa5fe6bc53107559b2c933c2543e3467713e
Martin Mitas <mity@morous.org>
2015-05-14 22:14:20 +0200

    dsa.c: Make DSA to reallocate less frequently.

--- src/dsa.c
+++ src/dsa.c
@@ -32,7 +32,7 @@
 #define DSA_IS_COMPACT(dsa)             (dsa->capacity == 0xffff)
 
 #define DSA_MAX_ITEM_SIZE               32
-#define DSA_DEFAULT_GROW_SIZE(size)     (MC_MAX(8, (size) / 16))
+#define DSA_DEFAULT_GROW_SIZE(size)     (MC_MAX(8, (size) / 4))
 #define DSA_DEFAULT_SHRINK_SIZE(size)   (2*DSA_DEFAULT_GROW_SIZE(size))
 
 
