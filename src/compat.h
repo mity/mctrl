d9891f55ed66b12a2e969015c76b814385d536c2
Martin Mitáš <mity@morous.org>
2015-09-17 13:49:44 +0200

    src/compat.h: Replace tab with spaces.

--- src/compat.h
+++ src/compat.h
@@ -143,7 +143,7 @@
     #endif
 
     /* MSVC does not know roundf() */
-	#include <math.h>
+    #include <math.h>
     static inline float roundf(float x)
         { return x >= 0.0f ? floorf(x + 0.5f) : ceilf(x - 0.5f); }
 
