1c12f912ef91bbe84a0311608641687eccf30a6d
Martin Mitas <mity@morous.org>
2016-04-28 00:09:40 +0200

    misc.h: Fix mc_clip_set() if HDC has LAYOUT_RTL.
    This fixes painting of grid control with WS_EX_LAYOUTRTL.

--- mctrl/misc.h
+++ mctrl/misc.h
@@ -449,7 +449,6 @@ mc_clip_set(HDC dc, LONG left, LONG top, LONG right, LONG bottom)
     HRGN clip;
     POINT pt[2] = { {left, top}, {right, bottom} };
 
-    LPtoDP(dc, pt, 2);
     clip = CreateRectRgn(pt[0].x, pt[0].y, pt[1].x, pt[1].y);
     if(MC_ERR(clip == NULL)) {
         MC_TRACE("mc_clip_set: CreateRectRgn() failed.");
