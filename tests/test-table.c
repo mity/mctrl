232663e68fb9bd7176f58c5547622631f59b1bdd
Martin Mitas <mity@morous.org>
2018-11-03 22:44:24 +0200

    test-table.c: Add some tests for testing table module.

---
+++ tests/test-table.c
@@ -0,0 +1,154 @@
+/*
+ * Unit tests verifying table data model.
+ * Copyright (c) 2018 Martin Mitas
+ */
+
+
+#include "acutest.h"
+#include <windows.h>
+#include <mCtrl/table.h>
+
+
+/***************
+ *** Helpers ***
+ ***************/
+
+static MC_HTABLE
+create_and_populate(int cols, int rows)
+{
+    MC_HTABLE table;
+    MC_TABLECELLA cell;
+    int c, r;
+    char buffer[32];
+
+    table = mcTable_Create(cols, rows, 0);
+    TEST_CHECK(table != NULL);
+
+    r = MC_TABLE_HEADER;
+    for(c = 0; c < cols; c++) {
+        sprintf(buffer, "[ %d, %d ]", c, r);
+        cell.fMask = MC_TCMF_TEXT | MC_TCMF_PARAM;
+        cell.pszText = buffer;
+        cell.lParam = MAKELPARAM(c, r);
+
+        TEST_CHECK(mcTable_SetCellA(table, c, r, &cell) == TRUE);
+    }
+
+    c = MC_TABLE_HEADER;
+    for(r = 0; r < rows; r++) {
+        sprintf(buffer, "[ %d, %d ]", c, r);
+        cell.fMask = MC_TCMF_TEXT | MC_TCMF_PARAM;
+        cell.pszText = buffer;
+        cell.lParam = MAKELPARAM(c, r);
+
+        TEST_CHECK(mcTable_SetCellA(table, c, r, &cell) == TRUE);
+    }
+
+    for(r = 0; r < rows; r++) {
+        for(c = 0; c < cols; c++) {
+            sprintf(buffer, "[ %d, %d ]", c, r);
+            cell.fMask = MC_TCMF_TEXT | MC_TCMF_PARAM;
+            cell.pszText = buffer;
+            cell.lParam = MAKELPARAM(c, r);
+
+            TEST_CHECK(mcTable_SetCellA(table, c, r, &cell) == TRUE);
+        }
+    }
+
+    return table;
+}
+
+static void
+check(MC_HTABLE table, int c, int r, LPARAM lp)
+{
+    MC_TABLECELLA cell;
+
+    cell.fMask = MC_TCMF_PARAM;
+
+    if(TEST_CHECK(mcTable_GetCellA(table, c, r, &cell) == TRUE))
+        TEST_CHECK_(cell.lParam == lp, " cell [%d, %d]", c, r);
+}
+
+
+/******************
+ *** Unit Tests ***
+ ******************/
+
+static void
+test_simple(void)
+{
+    MC_HTABLE table;
+
+    table = create_and_populate(4, 4);
+
+    check(table, MC_TABLE_HEADER, 0, MAKELPARAM(MC_TABLE_HEADER, 0));
+    check(table, 3, MC_TABLE_HEADER, MAKELPARAM(3, MC_TABLE_HEADER));
+
+    check(table, 0, 0, MAKELPARAM(0, 0));
+    check(table, 1, 2, MAKELPARAM(1, 2));
+    check(table, 3, 3, MAKELPARAM(3, 3));
+
+    mcTable_Release(table);
+}
+
+static void
+test_no_columns(void)
+{
+    MC_HTABLE table;
+
+    table = create_and_populate(0, 4);
+
+    check(table, MC_TABLE_HEADER, 0, MAKELPARAM(MC_TABLE_HEADER, 0));
+
+    mcTable_Release(table);
+}
+
+static void
+test_no_rows(void)
+{
+    MC_HTABLE table;
+
+    table = create_and_populate(4, 0);
+
+    check(table, 3, MC_TABLE_HEADER, MAKELPARAM(3, MC_TABLE_HEADER));
+
+    mcTable_Release(table);
+}
+
+static void
+test_resize(void)
+{
+    MC_HTABLE table;
+
+    table = create_and_populate(4, 4);
+
+    TEST_CHECK(mcTable_Resize(table, 3, 5) == TRUE);
+    TEST_CHECK(mcTable_ColumnCount(table) == 3);
+    TEST_CHECK(mcTable_RowCount(table) == 5);
+
+    check(table, MC_TABLE_HEADER, 0, MAKELPARAM(MC_TABLE_HEADER, 0));
+    check(table, 2, MC_TABLE_HEADER, MAKELPARAM(2, MC_TABLE_HEADER));
+
+    check(table, 0, 0, MAKELPARAM(0, 0));
+    check(table, 1, 2, MAKELPARAM(1, 2));
+
+    /* Check the new row is zeroed. */
+    check(table, MC_TABLE_HEADER, 4, MAKELPARAM(0, 0));
+    check(table, 0, 4, MAKELPARAM(0, 0));
+    check(table, 2, 4, MAKELPARAM(0, 0));
+
+    mcTable_Release(table);
+}
+
+
+/*****************
+ *** Test List ***
+ *****************/
+
+TEST_LIST = {
+    { "simple",         test_simple },
+    { "no-columns",     test_no_columns },
+    { "no-rows",        test_no_rows },
+    { "resize",         test_resize },
+    { 0 }
+};
